ce54e5ddf8a24e12c84574355b0c9f24e18879d8
Pieter Vanderpol <pjvanderpol@gmail.com>
2021-10-02 15:05:28 -0700

    update scripts

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 deno run --allow-read --allow-write './scripts/bundle.ts'
-deno compile --output 'build/check-licence-appts' --target 'x86_64-pc-windows-msvc' --allow-read --allow-net --allow-run --unstable index.ts
-deno compile --output 'build/check-licence-appts' --target  'x86_64-unknown-linux-gnu' --allow-read --allow-net --allow-run --unstable index.ts
+deno compile --output 'build/check-licence-appts-linux' --target 'x86_64-pc-windows-msvc' --allow-read --allow-net --allow-run --unstable index.ts
+deno compile --output 'build/check-licence-appts-windows' --target  'x86_64-unknown-linux-gnu' --allow-read --allow-net --allow-run --unstable index.ts
--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nodemon -e ".ts,.html,.json" -x "deno run --allow-run --allow-read --allow-write --unstable --allow-net index.ts --profile-path testFixtures/testProfile.json5 --environment development"
+deno run --watch --allow-run --allow-read --allow-write --unstable --allow-net index.ts --profile-path testFixtures/testProfile.json5 --environment development
